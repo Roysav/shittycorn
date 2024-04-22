deee436263a55f3d9e5cfeb2d7696c07468a10e0
roysav <roysav123@gmail.com>
2024-04-22 12:05:22 +0300

    edtied setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name="shittycorn",
-    version="0.0.1",
+    version="0.0.11",
     description="a shitty web server",
-    long_description="a simple wsgi compatible web server.",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
 )
